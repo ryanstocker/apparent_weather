5c18563626633013ace24bfd02aa97d449b0ebb7
Ryan Stocker <stocker.19@osu.edu>
2023-11-22 16:12:32 -0500

    additional gsub for bad image URLs from NWS

--- app/components/forecast_important_item_card_component.rb
+++ app/components/forecast_important_item_card_component.rb
@@ -12,6 +12,6 @@ class ForecastImportantItemCardComponent < ViewComponent::Base
     # sometimes the NWS service returns image URLs with ",<<number>>" in the URL
     # that return a 400 when accessed. Removing it results in a 200.
     # "small", "medium", and "large" are size options
-    @forecast_partial_day.icon.gsub(/\,\d/,"").gsub("small","large")
+    @forecast_partial_day.icon.gsub(/(,\d)|(\d)/, "").gsub("small", "large")
   end
 end
