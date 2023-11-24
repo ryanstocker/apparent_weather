f4aed014675d7b8a56189d813c882e535ee75050
Ryan Stocker <stocker.19@osu.edu>
2023-11-24 14:33:21 -0500

    add all attributes to forecast_partial_day spec

--- spec/models/forecast_partial_day_spec.rb
+++ spec/models/forecast_partial_day_spec.rb
@@ -29,7 +29,7 @@ RSpec.describe ForecastPartialDay, type: :model do
       windDirection: "NE",
       icon: "https://api.weather.gov/icons/land/day/ovc,2?size=small",
       shortForecast: "Cloudy",
-      detailedForecast: ""
+      detailedForecast: "Detailed Forecast"
     }
   end
 
@@ -37,6 +37,10 @@ RSpec.describe ForecastPartialDay, type: :model do
     it 'has the correct data' do
       expect(forecast_partial_day.short_forecast).to eq("Cloudy")
       expect(forecast_partial_day.temperature).to eq(42)
+      expect(forecast_partial_day.start_time).to eq(DateTime.parse("2023-11-20T10:00:00-05:00"))
+      expect(forecast_partial_day.end_time).to eq(DateTime.parse("2023-11-20T11:00:00-05:00"))
+      expect(forecast_partial_day.detailed_forecast).to eq("Detailed Forecast")
+      expect(forecast_partial_day.icon).to eq("https://api.weather.gov/icons/land/day/ovc,2?size=small")
     end
   end
 end
